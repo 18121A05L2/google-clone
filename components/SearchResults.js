8a5ab10deb541d93a436dcbf82cf4147dca96109
lucky2892000 <59156926+18121A05L2@users.noreply.github.com>
2022-09-04 16:39:44 +0530

    bug fix

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -4,21 +4,24 @@ import PaginationButtons from "./PaginationButtons";
 export default function SearchResults({ results }) {
   console.log(results);
   return (
-    <div className="m-2 b0rder-t-2 text-sm md:text-base">
-      <p className="text-gray-500  text-sm mb-2">
+    <div className="m-2 pt-3 border-t-2 text-sm ">
+      <p className="text-gray-500  text-sm mb-3">
         About {results?.searchInformation?.formattedTotalResults} in {""}(
         {results?.searchInformation?.searchTime}) seconds
       </p>
       {results.items?.map((item) => (
         <div className="max-w-2xl mb-4">
-          <a href={item.link}>
-            <p>{item.formattedUrl}</p>
-          </a>
-          <a href={item.link}>
-            <p className="text-blue-500 font-medium text-md md:text-xl">
-              {item.title}
-            </p>
-          </a>
+          <div className="group">
+            <a href={item.link}>
+              <p>{item.formattedUrl}</p>
+            </a>
+            <a href={item.link}>
+              <p className="text-blue-500 font-medium text-md md:text-xl group-hover:link">
+                {item.title}
+              </p>
+            </a>
+          </div>
+
           <div className="">{item.snippet}</div>
         </div>
       ))}
